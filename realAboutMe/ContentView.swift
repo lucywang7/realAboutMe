587aef491624bb5dcb433ce41f45f327eda5055a
Lucy <lucywang340@gmail.com>
2025-07-09 12:53:16 -0500

    Initial Commit

---
+++ realAboutMe/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  realAboutMe
+//
+//  Created by Scholar on 7/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("All about Lucy")
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.pink)
+            Spacer()
+            Image("Meee")
+                
+            }
+        }
+    }
+
+
+#Preview {
+    ContentView()
+}
---
+++ realAboutMe/realAboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  realAboutMeApp.swift
+//  realAboutMe
+//
+//  Created by Scholar on 7/9/25.
+//
+
+import SwiftUI
+
+@main
+struct realAboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
