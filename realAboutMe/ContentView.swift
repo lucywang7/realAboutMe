bd18c39a05d8c032a6d73e899366be8b1b946e82
Lucy <lucywang340@gmail.com>
2025-07-09 15:37:51 -0500

    all my code oops

--- realAboutMe/ContentView.swift
+++ realAboutMe/ContentView.swift
@@ -9,19 +9,49 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("All about Lucy")
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundColor(Color.pink)
-            Spacer()
-            Image("Meee")
+        ZStack{
+            Image("blue")
+                .resizable()
                 
+            
+            VStack {
+                Text("All about Lucy")
+                
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .foregroundColor(Color.yellow)
+                    .multilineTextAlignment(.center)
+           
+                
+    
+                Image ("meee")
+                    .renderingMode(.original)
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 300.0, height: 300.0)
+                Image("Image")
+                    .resizable(resizingMode: .stretch)
+                    .aspectRatio(contentMode: .fit)
+               
+                Spacer()
+                
+                
+                
+                Text("Hi I'm Lucy and I'm going to be a sophmore in highschool in the fall. I run cross country and play softball. I like eating fruit and bagels for breakfast. My favorite fast food place is chick fa lay")
+                Spacer()
             }
+            
         }
     }
+    
+    
+    
+}
 
 
 #Preview {
     ContentView()
+    
+    
 }
+    
